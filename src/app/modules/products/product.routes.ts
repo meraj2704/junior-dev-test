b716d755b66c8344eb307dfb2095dcbc17775b35
meraj2704 <mh1669101@gmail.com>
2024-12-05 11:08:09 +0600

    update code

--- src/app/modules/products/product.routes.ts
+++ src/app/modules/products/product.routes.ts
@@ -13,18 +13,9 @@ router.post(
 );
 
 router.get("/all-products", ProductController.getAllProducts);
-
 router.get("/product-details/:id", ProductController.getProductDetails);
-
 router.put("/add-to-wish-list/:id", ProductController.addOnWishlist);
 router.get("/all-wishList-products", ProductController.getAllWishListProducts);
-
-// router.put(
-//   "/product-update/:id",
-//   upload.single("file"),
-//   ProductController.productUpdate
-// );
-
 router.delete("/product-delete/:id", ProductController.productDelete);
 
 export const ProductRouter = router;
